16a23049e4fd086b63a2e3a4716bcdfbb6866afd
Aavhan Chatse <Aavhan.chatse@winjittechnologiespvtltd.local>
2025-04-15 11:08:27 +0530

    removed static links

--- lib/core/constants/external_links/external_links.dart
+++ lib/core/constants/external_links/external_links.dart
@@ -1,21 +1,4 @@
 class ExternalLinks {
-  static const TERMS_CONDITION = 'https://hoteljobber.com/terms-condition';
-  static const PRIVACY_POLICY = 'https://hoteljobber.com/privacy_policy.php';
-
-  static const FB_LINK = 'https://www.facebook.com/hoteljobber';
-  static const INSTA_LINK = 'https://www.instagram.com/hoteljobber_official/';
-  static const WHATS_APP_LINK = 'https://hoteljobber.com/follow';
-  static const DEMO_VIDEO_LINK =
-      'https://hoteljobber.com/hoteljobber-employer-app-demo';
-
-  static const CANDIDATE_APP_LINK =
-      "https://play.google.com/store/apps/details?id=com.hoteljobber.candidate";
-
-  static const CANDIDATE_APP_LINK_IOS =
-      "https://apps.apple.com/in/app/hotel-jobber-employer/id1574494959";
-
-  static const organisationLogoUrl =
-      "https://hoteljobber.com/public/uploads/organisation_logos/";
-  static const organisationAlbumUrl =
-      "https://hoteljobber.com/public/uploads/organisation_albums/";
+  static const TERMS_CONDITION = '';
+  static const PRIVACY_POLICY = '';
 }
